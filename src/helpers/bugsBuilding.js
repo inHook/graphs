b069fed21ae0d83261c03a3cde9ac5e740a3ea0b
Max <dufletor@gmail.com>
2020-04-26 00:38:31 +0300

    add test

--- src/helpers/bugsBuilding.js
+++ src/helpers/bugsBuilding.js
@@ -1,16 +1,23 @@
-import {BUG_KEYS, months, QUANTITY} from "../constants/bugKeys";
+import { BUG_KEYS, months, QUANTITY } from "../constants/bugKeys";
 
 // начальное преобразование JSON данных в объект для первоначального рендера графика
 export const bugsBuilding = (bugs = []) => {
-    const intervalObj = {};
-    const monthArray = [];
-    const monthArrayWithIndex = [];
-    const arrayForGraph = [];
-    const systemTypes = [];
-    const criticalTypes = [];
-    const keysBugs = Array.isArray(bugs) && bugs.length && Object.keys(bugs[0]);
+  const intervalObj = {};
+  const monthArray = [];
+  const monthArrayWithIndex = [];
+  const arrayForGraph = [];
+  const systemTypes = [];
+  const criticalTypes = [];
+  const keysBugs = Array.isArray(bugs) && bugs.length && Object.keys(bugs[0]);
 
-    Array.isArray(bugs) && bugs.sort((a, b) => new Date(a[BUG_KEYS.DATE_OF_CREATION]) - new Date(b[BUG_KEYS.DATE_OF_CREATION])).forEach((bug, index) => {
+  Array.isArray(bugs) &&
+    bugs
+      .sort(
+        (a, b) =>
+          new Date(a[BUG_KEYS.DATE_OF_CREATION]) -
+          new Date(b[BUG_KEYS.DATE_OF_CREATION])
+      )
+      .forEach((bug, index) => {
         const date = new Date(bug[BUG_KEYS.DATE_OF_CREATION]);
         const month = date.getMonth();
         const year = date.getFullYear();
@@ -19,54 +26,56 @@ export const bugsBuilding = (bugs = []) => {
         const critical = bug[BUG_KEYS.CRITICALITY];
 
         if (!systemTypes.includes(system)) {
-            systemTypes.push(system);
+          systemTypes.push(system);
         }
 
         if (!criticalTypes.includes(critical)) {
-            criticalTypes.push(critical);
+          criticalTypes.push(critical);
         }
 
         if (!monthArray.includes(monthAndYear)) {
-            monthArray.push(monthAndYear);
-            monthArrayWithIndex.push(`${index}_${monthAndYear}`);
+          monthArray.push(monthAndYear);
+          monthArrayWithIndex.push(`${index}_${monthAndYear}`);
 
-            const getIndex = monthArray.indexOf(monthAndYear);
-            const getKeyWithIndex = monthArrayWithIndex[getIndex];
+          const getIndex = monthArray.indexOf(monthAndYear);
+          const getKeyWithIndex = monthArrayWithIndex[getIndex];
 
-            intervalObj[getKeyWithIndex] = {
-                name: monthAndYear,
-                length: 0,
-                bugs: [],
-            };
+          intervalObj[getKeyWithIndex] = {
+            name: monthAndYear,
+            length: 0,
+            bugs: [],
+          };
         }
 
         const getIndex = monthArray.indexOf(monthAndYear);
         const getKeyWithIndex = monthArrayWithIndex[getIndex];
 
         intervalObj[getKeyWithIndex].bugs.push(bug);
-        intervalObj[getKeyWithIndex].length = intervalObj[getKeyWithIndex].bugs.length;
+        intervalObj[getKeyWithIndex].length =
+          intervalObj[getKeyWithIndex].bugs.length;
+      });
+
+  for (let key in intervalObj) {
+    arrayForGraph.push({
+      name: intervalObj[key].name,
+      [QUANTITY]: intervalObj[key].length,
+
+      bugs: intervalObj[key].bugs,
     });
+  }
 
-    for (let key in intervalObj) {
-        arrayForGraph.push({
-            name: intervalObj[key].name,
-            [QUANTITY]: intervalObj[key].length,
-            bugs: intervalObj[key].bugs,
-        })
-    }
-
-    return {
-        data: {
-            monthArray,
-            monthArrayWithIndex,
-            arrayForGraphRead: arrayForGraph,
-            arrayForGraphRender: arrayForGraph,
-            systemTypes,
-            criticalTypes,
-            monthFrom: monthArray[0],
-            monthTo: monthArray[monthArray.length - 1],
-            keysBugs,
-        },
-        intervalObj,
-    };
+  return {
+    data: {
+      monthArray,
+      monthArrayWithIndex,
+      arrayForGraphRead: arrayForGraph,
+      arrayForGraphRender: arrayForGraph,
+      systemTypes,
+      criticalTypes,
+      monthFrom: monthArray[0],
+      monthTo: monthArray[monthArray.length - 1],
+      keysBugs,
+    },
+    intervalObj,
+  };
 };
--- src/helpers/tableValidate.js
+++ src/helpers/tableValidate.js
@@ -1,21 +1,46 @@
-import {BUG_KEYS} from "../constants/bugKeys";
+import { BUG_KEYS } from "../constants/bugKeys";
 
 // валидация поиска в таблице по введённым значениям
 export const tableSearchValidate = (bug, searchValues) => {
-    const {ID, SYSTEM, SUMMARY, STATE, FOUND_AT, CRITICALITY, DEFECT_TYPE, DATE_OF_CREATION, DATE_OF_CHANGE, CLOSING_DATE, DETECTION_METHOD, REOPENS_AMOUNT} = BUG_KEYS;
+  const {
+    ID,
+    SYSTEM,
+    SUMMARY,
+    STATE,
+    FOUND_AT,
+    CRITICALITY,
+    DEFECT_TYPE,
+    DATE_OF_CREATION,
+    DATE_OF_CHANGE,
+    CLOSING_DATE,
+    DETECTION_METHOD,
+    REOPENS_AMOUNT,
+  } = BUG_KEYS;
 
-    if (bug[ID].toString().includes(searchValues[ID]) &&
-        bug[SYSTEM].includes(searchValues[SYSTEM]) &&
-        bug[SUMMARY].includes(searchValues[SUMMARY]) &&
-        bug[STATE].includes(searchValues[STATE]) &&
-        bug[FOUND_AT].includes(searchValues[FOUND_AT]) &&
-        bug[CRITICALITY].includes(searchValues[CRITICALITY]) &&
-        bug[DEFECT_TYPE].includes(searchValues[DEFECT_TYPE]) &&
-        (!bug[DATE_OF_CREATION] || (bug[DATE_OF_CREATION] && bug[DATE_OF_CREATION].includes(searchValues[DATE_OF_CREATION]))) &&
-        (!bug[DATE_OF_CHANGE] || (bug[DATE_OF_CHANGE] && bug[DATE_OF_CHANGE].includes(searchValues[DATE_OF_CHANGE]))) &&
-        (!bug[CLOSING_DATE] || (bug[CLOSING_DATE] && bug[CLOSING_DATE].includes(searchValues[CLOSING_DATE]))) &&
-        (!bug[DETECTION_METHOD] || (bug[DETECTION_METHOD] && bug[DETECTION_METHOD].includes(searchValues[DETECTION_METHOD]))) &&
-        (!bug[REOPENS_AMOUNT] || (bug[REOPENS_AMOUNT] && bug[REOPENS_AMOUNT].includes(searchValues[REOPENS_AMOUNT])))) {
-        return true;
-    }
+  if (
+    bug[ID].toString().includes(searchValues[ID]) &&
+    bug[SYSTEM].includes(searchValues[SYSTEM]) &&
+    bug[SUMMARY].includes(searchValues[SUMMARY]) &&
+    bug[STATE].includes(searchValues[STATE]) &&
+    bug[FOUND_AT].includes(searchValues[FOUND_AT]) &&
+    bug[CRITICALITY].includes(searchValues[CRITICALITY]) &&
+    bug[DEFECT_TYPE].includes(searchValues[DEFECT_TYPE]) &&
+    (!bug[DATE_OF_CREATION] ||
+      (bug[DATE_OF_CREATION] &&
+        bug[DATE_OF_CREATION].includes(searchValues[DATE_OF_CREATION]))) &&
+    (!bug[DATE_OF_CHANGE] ||
+      (bug[DATE_OF_CHANGE] &&
+        bug[DATE_OF_CHANGE].includes(searchValues[DATE_OF_CHANGE]))) &&
+    (!bug[CLOSING_DATE] ||
+      (bug[CLOSING_DATE] &&
+        bug[CLOSING_DATE].includes(searchValues[CLOSING_DATE]))) &&
+    (!bug[DETECTION_METHOD] ||
+      (bug[DETECTION_METHOD] &&
+        bug[DETECTION_METHOD].includes(searchValues[DETECTION_METHOD]))) &&
+    (!bug[REOPENS_AMOUNT] ||
+      (bug[REOPENS_AMOUNT] &&
+        bug[REOPENS_AMOUNT].includes(searchValues[REOPENS_AMOUNT])))
+  ) {
+    return true;
+  }
 };
